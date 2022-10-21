c662d7917c3159a48125b2f421090c2d42accfda
juanm <juanm@LAPTOP-91UVJHIB>
2022-10-21 02:34:27 -0500

    sidebarjs

--- Parcial2/SidebarJS/sidebarjs.js
+++ Parcial2/SidebarJS/sidebarjs.js
@@ -1,16 +1,10 @@
-var opct = elem.style.opacity;
-function clicked(color){
-    console.log(opct);
-    //var source = event.srcElement.id;
-    const elem = document.getElementById("sidebar");
-    //elem.style.background = color;
 
-    if (elem.style.opacity = "0" == "100") {
-        elem.style.opacity = "0";
-        console.log("C1");
-    }
-    if (elem.style.opacity == "0") {
-        elem.style.opacity = "100";
-        console.log("C2");
+
+function toggleSidebar(){
+    var x = document.getElementById("sidebar");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
     }
 }
