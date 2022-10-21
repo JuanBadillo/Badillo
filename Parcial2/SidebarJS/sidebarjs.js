75f26702afcc7c4761e47daa1748bed63df29e32
juanm <juanm@LAPTOP-91UVJHIB>
2022-10-21 16:50:28 -0500

    sidebarjs

--- Parcial2/SidebarJS/sidebarjs.js
+++ Parcial2/SidebarJS/sidebarjs.js
@@ -1,5 +1,3 @@
-
-
 function toggleSidebar(){
     var x = document.getElementById("sidebar");
     if (x.style.display === "none") {
@@ -7,4 +5,21 @@ function toggleSidebar(){
     } else {
       x.style.display = "none";
     }
+
+    const elem = document.getElementById("body");
+    elem.style.background = "white";
 }
+
+function cambiaFondo(newColor) {
+    var time = new Date();
+    var timestamp = "[" + time.getHours() + ":" + time.getMinutes() + ":" + time.getSeconds() + "]";
+    
+    const elem = document.getElementById("body");
+
+    elem.style.background = newColor;
+    elem.style.background = '-moz-linear-gradient(top, '+newColor+' 0%, #FFFFFF 50%, #FFFFFF 100%)';
+    elem.style.background = '-webkit-linear-gradient(top, '+newColor+' 0%, #FFFFFF 50%, #FFFFFF 100%)';
+    elem.style.background = 'linear-gradient(to bottom, '+newColor+' 0%, #FFFFFF 50%, #FFFFFF 100%)';
+
+    console.log(timestamp + "Cambio de Fondo a "+newColor);
+  }
