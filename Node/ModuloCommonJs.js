458730707cda2920af2f0bf746521485814c0de4
juanm <juanm@LAPTOP-91UVJHIB>
2022-11-09 23:37:58 -0600

    commit

---
+++ Node/ModuloCommonJs.js
@@ -0,0 +1,8 @@
+const modulo = require("./modulo");
+var cowsay = require("cowsay");
+
+console.log(cowsay.say({
+    text : "I'm a moooodule",
+    e : "oO",
+    T : "U "
+}));
---
+++ Node/ModuloJS.js
@@ -0,0 +1,8 @@
+
+const cuadrado = (valor) => {return valor*valor;}
+
+//console.log(module);
+
+//console.log(cuadrado(2));
+
+exports.cuadrado = cuadrado;
